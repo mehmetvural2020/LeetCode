ed7ef2134c842950c0c6b5206f2c1c3504fe1174
mehmetvural2020 <mehmetvurall@gmail.com>
2021-01-30 00:39:26 +0700

    Created first class

--- src/Stack/CrawlerLogFolder1598.java
+++ src/Stack/CrawlerLogFolder1598.java
@@ -10,4 +10,11 @@ git add .
 git status
 git commit -m"...."
 git push --set-upstream <git repo adresi> master
+
+
+Your members will have to manually update their local environments. We'll let them know when they visit the repository, or you can share these commands:
+
+git branch -m main <BRANCH>
+git fetch origin
+git branch -u origin/<BRANCH> <BRANCH>
  */
