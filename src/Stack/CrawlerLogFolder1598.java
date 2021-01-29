261d876a9bd0747de111d352737eb1f1d46c98be
mehmetvural2020 <mehmetvurall@gmail.com>
2021-01-30 00:24:18 +0700

    LeetCode questions solutions code

---
+++ src/Stack/CrawlerLogFolder1598.java
@@ -0,0 +1,13 @@
+package Stack;
+
+public class CrawlerLogFolder1598 {
+
+}
+/* git commands
+git init
+git status
+git add .
+git status
+git commit -m"...."
+git push --set-upstream <git repo adresi> master
+ */
