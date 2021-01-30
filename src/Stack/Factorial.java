2b963493a8a775cf6f3868a9433f26c72d7ee798
mehmetvural2020 <mehmetvurall@gmail.com>
2021-01-31 02:19:03 +0700

    Created Factorial class.

---
+++ src/Stack/Factorial.java
@@ -0,0 +1,20 @@
+package Stack;
+
+public class Factorial {
+    public static void main(String[] args) {
+        computeFactorialWithoutRecursion(5);
+        computeFactorialWithRecursion(5);
+
+    }
+
+    private static void computeFactorialWithoutRecursion(int factorialNumber) {
+        int Result = 1;
+        for(int i = factorialNumber; i > 0; i++ ) {
+
+        }
+    }
+
+    private static void computeFactorialWithRecursion(int factorialNumber) {
+    }
+
+}
